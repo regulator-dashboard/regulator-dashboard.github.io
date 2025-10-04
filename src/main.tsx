51a501e3680ce3335ea983541ffb37c68820bc14
regulator-dashboard <wenxinxixi@outlook.com>
2025-10-04 19:15:41 +1000

    init

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,29 @@ import './index.css';
 import AppRouter from './app/router';
 import { ConfigProvider, theme } from 'antd';
 import 'antd/dist/reset.css';
+import { worker } from './mock/browser';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Start MSW worker
+worker.start({
+  onUnhandledRequest: 'bypass',
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
-      <AppRouter />
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
+        <AppRouter />
+      </ConfigProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
